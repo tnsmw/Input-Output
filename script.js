b061fa53f51e2ae02266dd6280c80de2852f3a7a
tnsmw <wangm111@newschool.edu>
2021-04-30 10:16:16 -0400

    drinks icons update
    added pre-existing inputs for price + opacity 1 when clicked

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 
+function iced () {
+   var input2 = document.querySelector('#price');
+   input2.value = 2.65;
+   document.querySelector("#iced-coffee").style.opacity = 1;
+}
+
+function latte () {
+   var input2 = document.querySelector('#price');
+   input2.value = 3.65;
+   document.querySelector("#caffe-latte").style.opacity = 1;
+}
+
+function frapp () {
+   var input2 = document.querySelector('#price');
+   input2.value = 4.95;
+   document.querySelector("#matcha-frappuccino").style.opacity = 1;
+}
+
 function calculate () {
   var input1 = document.querySelector('#wallet').value;
   var input2 = document.querySelector('#price').value;
