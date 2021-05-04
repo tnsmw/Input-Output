fc2769c1730f3a213178df3b3206d03deda148e9
tnsmw <wangm111@newschool.edu>
2021-05-04 01:13:42 -0400

    idk

--- script.js
+++ script.js
@@ -32,10 +32,37 @@ function calculate () {
   }
 }
 
-function showdrinks () {
+/*function showdrinks () {
    console.log("showdrinks");
    // if input2.value = 3.65
    // var image
    // get total
    // show images depending on total (4 loop?)
+}*/
+
+
+function showDrinks() {
+    let result = +document.getElementById('result').value; // take value as a number
+    let drinks = document.getElementById('amount');
+
+    if (isNaN(result) || result < 1) { // move exit condition to top and exit early
+        alert("Not enough money.")
+        return;
+    }
+
+/*   var showDrinks = document.createElement("img");
+   document.querySelector(".container").append(showDrinks);
+   if input2.value = 2.65 {
+      showDrinks.img.src = "iced-coffee.png";
+   }
+   if input2.value = 3.65 {
+      showDrinks.img.src = "caffè-latte.png";
+   }
+   if input2.value = 4.95 {
+      showDrinks.img.src = "matcha-frappuccino.png";
+   }
+   for (var i = 0; i <= 5; i++) {
+
+   }
+*/
 }
